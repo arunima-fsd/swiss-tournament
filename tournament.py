06127a523eb0562367c14f05740846a7019dc083
Arunima Srivastava <arunimaasrivastava@gmail.com>
2017-01-03 21:13:27 +0530

    Write code for deleting and registering functions.

--- tournament.py
+++ tournament.py
@@ -8,29 +8,147 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=swiss_tournament")
+    return psycopg2.connect("host= 'localhost' user= 'arunimasrivastava'  dbname = 'tournament' ")
 
-def deleteTournament(tournamentId):
+
+
+def deleteTournament():
+    """Remove all the tournaments records"""
+    
     conn = connect()
     cur = conn.cursor()
-    cur.execute("DELETE FROM tournament WHERE id = %s;",(tournamentId))
+    cur.execute("DELETE FROM swiss_tournament.tournament;")
     conn.commit()
+    cur.close()
     conn.close()
+    
+def deleteTeams():
+    """Remove all the teams records"""
+    
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM swiss_tournament.teams;")
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+def deletePlayers():
+    """Remove all the player records from the database."""
+    
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM swiss_tournament.player_info;")
+    conn.commit()
+    cur.close()
+    conn.close()
+    
 
 
 def deleteMatches():
-    """Remove all the match records from the database."""
+    """Remove all the match records from the database. Deleting this automatically deletes all the records in 
+    'match' table as per the cascade deletion """
+    
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM swiss_tournament.match_schedule;")
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
-def deletePlayers():
-    """Remove all the player records from the database."""
 
 
-def countPlayers():
+
+def deleteCountry():
+    """Removes all the country details. Country id is foreign key to two tables 'Tournament' and 'Team' 
+    Deletion is only possible when there is no data in those table"""
+    
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM swiss_tournament.country;")
+    conn.commit()
+    cur.close()
+    conn.close()    
+    
+
+
+def countPlayers(tournamentId):
     """Returns the number of players currently registered."""
+    
+    conn = connect()
+    cur = conn.cursor()
+    query = """SELECT count(*)
+       FROM
+           swiss_tournament.player_info o, swiss_tournament.teams t
+       WHERE 
+           o.team_id = t.id 
+       AND
+           t.tournament_id = %s ;"""
+    
+    cur.execute(query, (tournamentId,))
+    player_count = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    
+    return player_count
+    
+    
+    
+    
+def registerCountry(name):
+    "Registers the country names belonging to various participating teams and players"
+    conn = connect()
+    cur = conn.cursor()
+    query = "INSERT INTO swiss_tournament.country( name) VALUES ( %s ) RETURNING id;"
+    cur.execute(query, (name, ))
+    country_id = cur.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return country_id    
+    
+    
+
+def registerTournament(name, start_date, country_id):
+    """ Adds a tournament into the database. 
+    Args:
+    name: Name of the tournament (mandatory)
+    start_date: The date when the tournament begins
+    country_id: the Id of the country where it is taking place.(mansatory)
+    
+    Returns the tournament Id of the currently registered tournament"""
+    
+    conn = connect()
+    cur = conn.cursor()
+    query = "INSERT INTO swiss_tournament.tournament (start_date, name, origin_country) VALUES (%s, %s, %s) RETURNING id;"
+    cur.execute(query, (start_date, name, country_id))
+    tournament_id = cur.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return tournament_id
 
 
-def registerPlayer(name):
+
+
+def registerTeam(name, tournament_id, country_id ):
+    """ Registers a particular team and returns its ID
+    Args: 
+    name: Team name (mandatory)
+    tournament_id: (mandatory
+    country_id: The country name where it belongs(mandatory)"""
+    
+    conn = connect()
+    cur = conn.cursor()
+    query = "INSERT INTO swiss_tournament.teams( tournament_id, name, country) VALUES (%s, %s, %s) RETURNING id;"
+    cur.execute(query, (tournament_id, name, country_id))
+    team_id = cur.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return team_id    
+    
+    
+
+
+def registerPlayer(name, team_id):
     """Adds a player to the tournament database.
   
     The database assigns a unique serial id number for the player.  (This
@@ -39,6 +157,22 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    full_name = name.split()
+    first_name = full_name[0]
+    if len(full_name) == 2:
+        last_name = full_name[1]
+        query = "INSERT INTO swiss_tournament.player_info( first_name, last_name, team_id) VALUES (  %s, %s, %s );"
+        data = ( first_name, last_name, team_id)
+    else:
+        query = "INSERT INTO swiss_tournament.player_info( first_name, team_id) VALUES (  %s, %s);"
+        data = ( first_name, team_id)
+    
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(query, data)
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 def playerStandings():
